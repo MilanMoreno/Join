b7addeddcc94265dc4ec4efa359a8970df34475b
MilanMoreno <st_sra@protonmail.com>
2024-10-10 14:27:12 +0200

    contacts.js

--- js/contact_Mobile.js
+++ js/contact_Mobile.js
@@ -1,23 +1,23 @@
-function checkUserMaxWidth() {
+function checkScreenSize() {
     const mobileWidth = 800;
-  
+
     if (window.matchMedia(`(max-width: ${mobileWidth}px)`).matches) {
-        document.getElementById('mobileContactReturn').classList.remove('displayNone');
-        document.getElementById('contactList').classList.add('displayNone');
-        document.getElementById('contentSection').classList.remove('dNone');
+        document.getElementById('responsiveContactBackButton').classList.remove('displayNone');
+        document.getElementById('mycontacts').classList.add('displayNone');
+        document.getElementById('content-area').classList.remove('dNone');
     }
-  }
-  
-  window.addEventListener('resize', hideMobileAssets)
-  
-  function hideMobileAssets() {
+}
+
+window.addEventListener('resize', concealMobileElements)
+
+function concealMobileElements() {
     if (window.innerWidth > 800) {
-        document.getElementById('mobileContactReturn').classList.add('displayNone');
+        document.getElementById('responsiveContactBackButton').classList.add('displayNone');
         document.getElementById('return_mobilePopUp').classList.add('displayNone');
         document.getElementById('return_editMobilePopUp').classList.add('displayNone');
     } else if (window.innerWidth < 800) {
         document.getElementById('return_mobilePopUp').classList.remove('displayNone');
         document.getElementById('return_editMobilePopUp').classList.remove('displayNone');
-        document.getElementById('mobileContactReturn').classList.remove('displayNone');
+        document.getElementById('responsiveContactBackButton').classList.remove('displayNone');
     }
-  }
+}
